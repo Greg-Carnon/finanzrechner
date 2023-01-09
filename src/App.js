5297dad2dbafcd1739f668ad5b13850e76619834
Greg Carnon <krafttraining.coach@gmail.com>
2023-01-09 21:01:30 +0100

    bugfix

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 
 export default function App() {
   // Declare state variables for monthly salary and fixed costs
-  const [salary, setSalary] = useState(0);
-  const [fixedCosts, setFixedCosts] = useState(0);
-  const [daySalary, setDaysSalary] = useState(0);
+  const [salary, setSalary] = useState(3293);
+  const [fixedCosts, setFixedCosts] = useState(1620);
+  const [daySalary, setDaysSalary] = useState(30);
   const [spendingsDayOne, setSpendingsDayOne] = useState(0);
   const [spendingsDayTwo, setSpendingsDayTwo] = useState(0);
   const [spendingsDayThree, setSpendingsDayThree] = useState(0);
@@ -97,15 +97,15 @@ export default function App() {
       <h1>Tagesgehalt Rechner</h1>
       <p>
         Monatliches Gehalt: €
-        <input type="number" value={salary} defaultValue={3293} onChange={e => setSalary(e.target.value)} />
+        <input type="number" value={salary} onChange={e => setSalary(e.target.value)} />
       </p>
       <p>
         Monatliche Fixkosten: €
-        <input type="number" value={fixedCosts} defaultValue={1620} onChange={e => setFixedCosts(e.target.value)} />
+        <input type="number" value={fixedCosts} onChange={e => setFixedCosts(e.target.value)} />
       </p>
       <p>
         Tage bis nächstes Gehalt:
-        <input type="number" value={daySalary} defaultValue={30} onChange={e => setDaysSalary(e.target.value)} />
+        <input type="number" value={daySalary} onChange={e => setDaysSalary(e.target.value)} />
       </p>
       <button onClick={calculateBudget}>Verbleibendes Gehalt berechnen</button>
       <p>
